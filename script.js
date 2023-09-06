9654735bc6a4eec1a1d98053fae7abdf9a53641d
Vizziona <138292424+vizziona@users.noreply.github.com>
2023-09-05 18:56:12 -0700

    Javascript connecting Front and Backend

---
+++ script.js
@@ -0,0 +1,6 @@
+const script = document.createElement('script');
+script.src = 'https://app.omnimind.ai/widgets/search-widget/search-widget.js';
+script.setAttribute('data-prop-widget-id', 'cf4007d5-6ae9-4e30-b029-af4b6929782f');
+script.setAttribute('data-prop-project-id', '858728de-6bc1-4621-b87d-5085853951b0');
+script.setAttribute('data-prop-is-dark-mode', true);
+document.getElementById('widget-container-search-widget').appendChild(script);
